d33b242852969f38bc29fdac21e451e667173a04
toakm13 <toakm13@gmail.com>
2026-05-09 20:47:09 +0530

    Update video.py

--- video.py
+++ video.py
@@ -1,31 +1,18 @@
 from moviepy.editor import ImageClip
 from datetime import datetime
 
-# -----------------------------------
-# DATE
-# -----------------------------------
-
-now = datetime.now()
-date_text = now.strftime("%Y-%m-%d")
-
-image_file = f"post_{date_text}.png"
-video_file = f"reel_{date_text}.mp4"
-
-# -----------------------------------
-# CREATE REEL
-# -----------------------------------
+image_file = f"post_{datetime.now().strftime('%Y-%m-%d')}.png"
 
 clip = (
     ImageClip(image_file)
     .set_duration(6)
-    .resize((1080, 1920))
+    .resize(height=1920)
 )
 
 clip.write_videofile(
-    video_file,
+    "reel.mp4",
     fps=24,
-    codec="libx264",
-    audio=False,
+    codec="libx264"
 )
 
-print(f"✅ Reel created: {video_file}")
+print("Reel video created")
